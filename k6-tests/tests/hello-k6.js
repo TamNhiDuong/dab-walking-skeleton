bd181489c5fe78f59a5f746b068132a0ecae5de7
TamNhiDuong <yanxinyee@gmail.com>
2025-09-29 16:43:30 +0300

    Client performance test with k6

--- k6-tests/tests/hello-k6.js
+++ k6-tests/tests/hello-k6.js
@@ -2,29 +2,29 @@ import { browser } from "k6/browser";
 import http from "k6/http";
 import { check } from "k6";
 
-export const options = {
-    // scenarios: {
-    //     client: {
-    //         vus: 2,
-    //         duration: "10s",
-    //         executor: "constant-vus",
-    //         exec: "loadPage",
-    //         options: {
-    //             browser: {
-    //                 type: "chromium",
-    //             },
-    //         },
-    //     },
-    //     server: {
-    //         vus: 2,
-    //         duration: "10s",
-    //         executor: "constant-vus",
-    //         exec: "getServerRoot",
-    //     },
-    // },
-    vus: 5,
-    duration: "10s",
-};
+// export const options = {
+//     scenarios: {
+//         client: {
+//             vus: 5,
+//             duration: "30s",
+//             executor: "constant-vus",
+//             exec: "loadPage",
+//             options: {
+//                 browser: {
+//                     type: "chromium",
+//                 },
+//             },
+//         },
+//         server: {
+//             vus: 2,
+//             duration: "10s",
+//             executor: "constant-vus",
+//             exec: "getServerRoot",
+//         },
+//     },
+//     vus: 5,
+//     duration: "10s",
+// };
 
 // export const loadPage = async () => {
 //     const page = await browser.newPage();
@@ -45,19 +45,51 @@ export const options = {
 //     });
 // };
 
-export default async () => {
-    const user = {
-        name: `User ${Math.random()}`,
-    };
+// export default async () => {
+//     const user = {
+//         name: `User ${Math.random()}`,
+//     };
 
-    const url = "http://traefik:8000/users";
-    const res = http.post(url, JSON.stringify(user), {
-        headers: {
-            "Content-Type": "application/json",
+//     const url = "http://traefik:8000/users";
+//     const res = http.post(url, JSON.stringify(user), {
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//     });
+
+//     check(res, {
+//         "status is 202": (r) => r.status === 202,
+//     });
+// };
+
+// Client test
+export const options = {
+    scenarios: {
+        client: {
+            vus: 5,
+            duration: "30s",
+            executor: "constant-vus",
+            options: {
+                browser: {
+                    type: "chromium",
+                },
+            },
         },
-    });
-
-    check(res, {
-        "status is 202": (r) => r.status === 202,
-    });
+    },
 };
+
+
+export default async () => {
+    const page = await browser.newPage();
+    await page.goto("http://client:4321/");
+
+    try {
+        for (let i = 1; i < 5; i++) {
+            await page.locator('//button[text()="Add item"]').click();
+            await page.locator(`//li[text()="Item ${i}"]`).isVisible();
+        }
+    } finally {
+        await page.close();
+    }
+};
+
